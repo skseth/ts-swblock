b607d1e98ae3f9c23e80ae98633f739c61f6a318
Samir Seth <samirkseth@gmail.com>
2022-06-14 00:52:34 +0530

    added tree-shaking

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,10 +1,28 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+  },
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+      terserOptions: {
+          compress: {
+              defaults: false,
+              unused: true
+          },
+          mangle: false,
+          format: {
+              comments: 'all'
+          }
+      }
+  })]
   },
 });
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -3,5 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    usedExports: true
+  },
   devtool: 'inline-source-map',
 });
